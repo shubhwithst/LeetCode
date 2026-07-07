70d247e1656eab40cced9dbeac5e03f386c3dc16
shubhwithst <56071725+shubhwithst@users.noreply.github.com>
2026-07-07 13:03:35 +0530

    Time: 1 ms (94.68%) | Memory: 46.3 MB (92.07%) - LeetSync

--- 4354-unique-middle-element/unique-middle-element.java
+++ 4354-unique-middle-element/unique-middle-element.java
@@ -3,9 +3,12 @@ class Solution {
         int mid = nums.length / 2;
         int cnt = 0;
         for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == nums[mid]) cnt++;
+            if (nums[i] == nums[mid])
+                cnt++;
         }
-        if(cnt>1) return false;
-        else return true;
+        if (cnt > 1)
+            return false;
+        else
+            return true;
     }
 }
