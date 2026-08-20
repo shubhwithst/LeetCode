2cc59ad1e17dc855b7a7f86a74316da629cdeb49
shubhwithst <56071725+shubhwithst@users.noreply.github.com>
2026-08-20 20:51:42 +0530

    Time: 6 ms (72.47%) | Memory: 45 MB (47.39%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,12 @@
 class Solution {
     int subsetSum(int[] arr, int len, int sum) {
         int[][] t = new int[len + 1][sum + 1];
-        // t[0][0] = 1;
-        for (int i = 0; i < len + 1; i++) {
-            for (int j = 0; j < sum + 1; j++) {
-                if (i == 0) {
-                    t[i][j] = 0;
-                }
-                if (j == 0) {
-                    t[i][j] = 1;
-                }
-            }
+        t[0][0] = 1;
+        for (int i = 1; i < len + 1; i++)
+            t[i][0] = 1;
+        for (int i = 1; i < sum + 1; i++) {
+            t[0][i] = 0;
         }
-        
         for (int i = 1; i < len + 1; i++) {
             for (int j = 0; j < sum + 1; j++) {
                 if (arr[i - 1] <= j) {
@@ -27,12 +21,14 @@ class Solution {
     }
 
     public int findTargetSumWays(int[] nums, int target) {
-       int sum = 0, n = nums.length;
+        int sum = 0, n = nums.length;
         for (int i = 0; i < n; i++) {
             sum += nums[i];
         }
-        if (Math.abs(target) > sum) return 0;
-        if ((target + sum) % 2 != 0) return 0;
+        if (Math.abs(target) > sum)
+            return 0;
+        if ((target + sum) % 2 != 0)
+            return 0;
         int actualSum = (target + sum) / 2;
         return subsetSum(nums, n, actualSum);
     }
