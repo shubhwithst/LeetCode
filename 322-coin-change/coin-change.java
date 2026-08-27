fa50e3f9d466668208c47d559ea9d3035ec4c8cb
shubhwithst <56071725+shubhwithst@users.noreply.github.com>
2026-08-27 22:07:55 +0530

    Time: 19 ms (39.52%) | Memory: 46.9 MB (31.11%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length, x = (int) 1e9 - 1;
+        int[][] t = new int[n + 1][amount + 1];
+        for (int[] row : t) {
+            Arrays.fill(row, -1);
+        }
+        t[0][0] = x;
+        for (int i = 1; i < amount + 1; i++) {
+            t[0][i] = x;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            t[i][0] = 0;
+        }
+        for (int i = 1; i < amount + 1; i++) {
+            if (i % coins[0] == 0) {
+                t[1][i] = i / coins[0];
+            } else {
+                t[1][i] = x;
+            }
+        }
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j < amount + 1; j++) {
+                if (coins[i - 1] <= j) {
+                    t[i][j] = Math.min(t[i - 1][j], 1 + t[i][j - coins[i - 1]]);
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+        return t[n][amount] == x ? -1 : t[n][amount];
+    }
+}
