47125e6ef873b575b178cf78ac1c45aefd4383c8
shubhwithst <56071725+shubhwithst@users.noreply.github.com>
2026-09-22 23:29:41 +0530

    Time: 76 ms (71.38%) | Memory: 98.2 MB (58.49%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -9,9 +9,9 @@ public class Solution {
         Arrays.sort(pair, (a, b) -> Integer.compare(b[0], a[0]));
 
         int fleets = 1;
-        double prevTime = (double)(target - pair[0][0]) / pair[0][1];
+        double prevTime = (double) (target - pair[0][0]) / pair[0][1];
         for (int i = 1; i < n; i++) {
-            double currTime = (double)(target - pair[i][0]) / pair[i][1];
+            double currTime = (double) (target - pair[i][0]) / pair[i][1];
             if (currTime > prevTime) {
                 fleets++;
                 prevTime = currTime;
