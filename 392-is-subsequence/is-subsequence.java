1020d604f3fee998c655d226212cc1170debe9d7
shubhwithst <56071725+shubhwithst@users.noreply.github.com>
2026-09-26 21:17:57 +0530

    Time: 9 ms (1.98%) | Memory: 43.9 MB (6.13%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,17 +1,14 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        if(s.length() == 0) return true;
-
-        int i = 0, j = 0;
-        while(i < s.length() && j < t.length()){
-            if(s.charAt(i) == t.charAt(j)){
-                i++;
-                j++;
-            }else{
-                j++;
+   public boolean isSubsequence(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 1; j < t.length() + 1; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-        if(i == s.length())return true;
-        else return false;
+        return dp[s.length()][t.length()] == s.length();
     }
 }
